0112d61f96f6c5738182aba0ce487d491ea9bd64
Suran Warnakulasooriya <68828123+suranwarnakulasooriya@users.noreply.github.com>
2021-09-21 20:59:04 -0400

    Update main.py

--- LaylaII/main.py
+++ LaylaII/main.py
@@ -8,15 +8,5 @@ from commands_help import *
 async def on_ready():
     print('\nLayla II is online.\n')
 
-#@client.event
-#async def on_member_join(member):
-#    U[member] = User()
-
-#@client.event
-#async def on_message(ctx):
-#    if ctx.author in U:
-#        if U[ctx.author].valid():
-#            U[ctx.author].xp += 1
-
 if __name__ == '__main__':
     client.run(Bot.token)
